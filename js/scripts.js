dffc48006ef758eea0c36901cfe7cfce47676197
Nick Kardish and Pablo Barreyro <nick@>
2018-04-04 11:49:34 -0700

    add game logic for handling first player turn

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,10 @@ function Hand() {
   this.total = 0;
 };
 
+function Game() {
+  this.currentPlayer = Math.floor((Math.random() * 2) + 1);
+};
+
 Turn.prototype.addRoll = function() {
   var roll = 0;
 
@@ -20,7 +24,7 @@ Turn.prototype.addRoll = function() {
     var newRoll = new Die(roll);
 
     if(newRoll.value !== 1){
-    this.runningTotal.push(newRoll.value);
+    this.runningTotal.unshift(newRoll.value);
     } else {
       this.rollAgain = false;
     }
@@ -39,20 +43,66 @@ Hand.prototype.updateScore = function(number) {
   this.total += number;
 };
 
+Game.prototype.changePlayer = function() {
+  if (this.currentPlayer === 1) {
+    this.currentPlayer = 2;
+  } else {
+    this.currentPlayer = 1;
+  }
+}
+
 
 //user logic
 $(document).ready(function(){
-  $("#formOne").submit(function(event){
-    event.preventDefault();
+  // $("#formOne").submit(function(event){
+  //   event.preventDefault();
+  //
+  //   $("#mode-selection").hide();
+  //   var mode = $("input:radio[name=mode]:checked").val();
+  //   //put conditional logic for setting up vs computer
+  //
+  //   $("#game-board").toggleClass('hide');
+  //   $("#game-board").attr('display', 'flex');
+  //   $("#game-board").attr('flex-direction', 'row');
+  //
+  // });
 
-    $("#mode-selection").hide();
-    var mode = $("input:radio[name=mode]:checked").val();
-    //put conditional logic for setting up vs computer
+  var newGame = new Game();
+  console.log(newGame.currentPlayer);
+  var playerOneHand = new Hand();
+  var playerTwoHand = new Hand();
 
-    $("#game-board").toggleClass('hide');
-    $("#game-board").attr('display', 'flex');
-    $("#game-board").attr('flex-direction', 'row');
+  if (newGame.currentPlayer === 1) {
+    $("#player-board-two button.roll").hide();
+    $("#player-board-two button.hold").hide();
+
+    var currentTurn = new Turn();
+    var turnScore = 0;
+    var runningTotal = currentTurn.runningTotal;
+
+  } else {
+    $("#player-board-one button.roll").hide();
+    $("#player-board-one button.hold").hide();
+
+    var currentTurn = new Turn();
+    var turnScore = 0;
+    var runningTotal = currentTurn.runningTotal;
+  }
+
+  $("button.roll").click(function() {
+    currentTurn.addRoll();
+    if (currentTurn.rollAgain === true){
+      $("ul#player-one-rolls").append("<li>" + runningTotal[0] + "</li>");
+      turnScore = currentTurn.sumArray();
+      $("#turn-score").text(turnScore);
+    } else {
+      $("ul#player-one-rolls").append("<li>Bust!</li>");
+      $("#turn-score").text("Bust!");
+    }
   });
 
-
+  $("button.hold").click(function(){
+    handScore.updateScore(turnScore);
+    $("#player1-score").text(handScore.total);
+  });
 });
